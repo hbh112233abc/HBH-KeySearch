9f417a400dac65242c25f5c05c7580f43573d056
huangbinghe <hbh112233abc@163.com>
2019-11-05 14:16:15 +0800

    Update key_search.py

--- key_search.py
+++ key_search.py
@@ -7,23 +7,23 @@ import urllib.parse
 class KeySearchCommand(sublime_plugin.TextCommand):
     def run(self, edit,driver=""):
         # 配置信息
-        print('input driver:',driver)
+        #print('input driver:',driver)
         conf = sublime.load_settings("HBH-KeySearch.sublime-settings")
         if not driver:
             driver = conf.get('default_driver','baidu')
-        print('select driver:',driver)
+        #print('select driver:',driver)
         search_format_conf = conf.get('search_format',{})
-        print('search_format_conf:',search_format_conf)
+        #print('search_format_conf:',search_format_conf)
         search_format = search_format_conf.get(driver,'https://www.baidu.com/s?wd={key}')
-        print('search_format:',search_format)
+        #print('search_format:',search_format)
         # 选中的字符串
         sels = self.view.sel()
         sel = sels[0]
-        print(sel)
+        #print(sel)
         keywords = self.view.substr(sels[0])
         keywords = urllib.parse.quote(keywords)
-        print('keywords:',keywords)
+        #print('keywords:',keywords)
         # 打开浏览器搜索引擎 搜索该文本
         url = search_format.replace('{key}',keywords)
-        print('url:',url)
+        #print('url:',url)
         webbrowser.open_new_tab(url)
